4491c19fdbdf342a90e4aadf5c7abd7a2322ddfc
Ian Stewart <istewart@kollective.com>
2025-04-13 12:35:08 -0700

    build fix

--- protoc-gen-java-records/build.gradle.kts
+++ protoc-gen-java-records/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
   application
+  jacoco
   `maven-publish`
   signing
   id("com.gradleup.shadow") version "8.3.6"
@@ -32,17 +33,19 @@ testing {
         }
   }
 }
+
 tasks.test {
-    finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
-    // systemProperty("log4j2.configurationFile", "log4j2-test.xml")
-    // systemProperty("sun.io.serialization.extendedDebugInfo", "true")
+  finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
+  // systemProperty("log4j2.configurationFile", "log4j2-test.xml")
+  // systemProperty("sun.io.serialization.extendedDebugInfo", "true")
 }
+
 tasks.jacocoTestReport {
-    dependsOn(tasks.test) // tests are required to run before generating the report
-    reports {
-        xml.required.set(true)
-        csv.required.set(true)
-    }
+  dependsOn(tasks.test) // tests are required to run before generating the report
+  reports {
+    xml.required.set(true)
+    csv.required.set(true)
+  }
 }
 
 spotless {
