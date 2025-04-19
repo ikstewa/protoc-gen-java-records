e088a6b36cffada7e702cc88c493c0b89df132a3
Ian Stewart <istewart@kollective.com>
2025-04-19 14:35:05 -0700

    Support basic scalar types

---
+++ protoc-gen-java-records/src/test/java/io/ikstewa/grpc/protoc/javarecords/ScalarTypesTest.java
@@ -0,0 +1,57 @@
+//
+// Copyright 2025 Ian Stewart
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+package io.ikstewa.grpc.protoc.javarecords;
+
+import com.google.common.truth.Truth;
+import io.test.protoc.ScalarTypesOuterClassRecords.OptionalScalarTypes;
+import io.test.protoc.ScalarTypesOuterClassRecords.ScalarTypes;
+import org.junit.jupiter.api.Test;
+
+class ScalarTypesTest {
+
+  @Test
+  void basic_test() {
+    Truth.assertThat(io.test.protoc.ScalarTypesOuterClassRecords.ScalarTypes.class).isNotNull();
+    Truth.assertThat(io.test.protoc.ScalarTypesOuterClassRecords.ScalarTypes.class.isRecord())
+        .isTrue();
+    Truth.assertThat(
+            new ScalarTypes(1.1d, 1.1f, 1, 1l, 2, 2l, 3, 3l, 4, 4l, 5, 5l, true, "string_value"))
+        .isEqualTo(
+            new ScalarTypes(1.1d, 1.1f, 1, 1l, 2, 2l, 3, 3l, 4, 4l, 5, 5l, true, "string_value"));
+  }
+
+  @Test
+  void optional_test() {
+    Truth.assertThat(io.test.protoc.ScalarTypesOuterClassRecords.OptionalScalarTypes.class)
+        .isNotNull();
+    Truth.assertThat(
+            io.test.protoc.ScalarTypesOuterClassRecords.OptionalScalarTypes.class.isRecord())
+        .isTrue();
+    Truth.assertThat(
+            new OptionalScalarTypes(
+                1.1d, 1.1f, 1, 1l, 2, 2l, 3, 3l, 4, 4l, 5, 5l, true, "string_value"))
+        .isEqualTo(
+            new OptionalScalarTypes(
+                1.1d, 1.1f, 1, 1l, 2, 2l, 3, 3l, 4, 4l, 5, 5l, true, "string_value"));
+    Truth.assertThat(
+            new OptionalScalarTypes(
+                null, null, null, null, null, null, null, null, null, null, null, null, null, null))
+        .isEqualTo(
+            new OptionalScalarTypes(
+                null, null, null, null, null, null, null, null, null, null, null, null, null,
+                null));
+  }
+}
